b8dbed0c8f72a60692d18a939386754ecc9f9a4e
Martin Golpashin <mgolpashin@gmail.com>
2017-03-16 00:05:48 +0100

    improved file margin top

--- app/src/main/java/de/martingolpashin/sensor_record/fragments/FileFragment.java
+++ app/src/main/java/de/martingolpashin/sensor_record/fragments/FileFragment.java
@@ -24,10 +24,7 @@ public class FileFragment extends Fragment {
     @ViewById
     RecyclerView file_list;
 
-
-    public FileFragment() {
-        // Required empty public constructor
-    }
+    public FileFragment() {}
 
     @AfterViews
     void init() {
@@ -41,7 +38,6 @@ public class FileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_files, container, false);
     }
 
