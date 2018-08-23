ba4e5c036c2e4c8ef1eee508cd92f8599fded84f
Martin Golpashin <mail@martingolpashin.de>
2018-08-23 20:52:13 +0200

    use new fusedLocation api to fetch gps data

--- app/src/main/java/de/martingolpashin/sensor_record/models/sensors/gps/GPS.java
+++ app/src/main/java/de/martingolpashin/sensor_record/models/sensors/gps/GPS.java
@@ -5,16 +5,15 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.CheckBox;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
 import org.androidannotations.annotations.EBean;
@@ -26,9 +25,8 @@ import de.martingolpashin.sensor_record.SensorRecordApplication;
 import de.martingolpashin.sensor_record.models.Sensor;
 
 @EBean
-public class GPS extends Sensor implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
+public class GPS extends Sensor {
     private Location lastKnownLocation;
-    GoogleApiClient googleApiClient;
 
     public GPS(final Context context) {
         super(context, "GPS", 100, GPSData.getHeadline());
@@ -46,14 +44,6 @@ public class GPS extends Sensor implements GoogleApiClient.ConnectionCallbacks, 
             }
         });
 
-
-        if (googleApiClient == null) {
-            googleApiClient = new GoogleApiClient.Builder(context)
-            .addConnectionCallbacks(this)
-            .addOnConnectionFailedListener(this)
-            .addApi(LocationServices.API)
-            .build();
-        }
     }
 
     @Override
@@ -73,35 +63,32 @@ public class GPS extends Sensor implements GoogleApiClient.ConnectionCallbacks, 
     public void setActive(boolean isActiveNew) {
         active = isActiveNew;
         if (active) {
-            googleApiClient.connect();
-        } else {
-            googleApiClient.disconnect();
+            connect();
         }
     }
 
-    @Override
-    public void onConnected(Bundle bundle) {
+    private void connect() {
         LocationRequest locationRequest = new LocationRequest();
         locationRequest.setInterval(this.interval);
+        locationRequest.setFastestInterval(this.interval);
+
+        LocationCallback callback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult result) {
+                lastKnownLocation = result.getLastLocation();
+            }
+        };
+
         Activity currentActivity = ((SensorRecordApplication) context.getApplicationContext()).getCurrentActivity();
+
         if(ContextCompat.checkSelfPermission(currentActivity,
                 Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            LocationServices.FusedLocationApi.requestLocationUpdates(this.googleApiClient, locationRequest, this);
+
+            FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(context);
+            client.requestLocationUpdates(locationRequest, callback, null);
         } else {
             getCheckBox().setChecked(false);
             ActivityCompat.requestPermissions(currentActivity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
     }
-
-
-    @Override
-    public void onLocationChanged(Location location) {
-        this.lastKnownLocation = location;
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {}
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {}
 }
